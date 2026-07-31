438ef04d97f0ef5a65bb75ecbd80edba27b8c385
Evan Nguyen <vudutminh@gmail.com>
2026-07-31 22:46:04 +1000

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int n = nums.Length;
+        int s = n * (n+1)/2;
+        int a = 0;
+        foreach (int num in nums) {
+            a += num;
+        }
+        int res = s-a;
+        return res;
+        
+    }
+}
