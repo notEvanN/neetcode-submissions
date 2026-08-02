6f89f5dccc134ab5374eda7d2035f39ee464cd87
Evan Nguyen <vudutminh@gmail.com>
2026-08-02 21:29:18 +1000

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int HammingWeight(uint n) {
+        int res =0;
+        for (int i =0;i<32;i++) {
+            if ((1<<i&n) != 0) {
+                res++;
+            }
+        }
+        return res;
+        
+    }
+}
