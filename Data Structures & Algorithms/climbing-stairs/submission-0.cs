2e9cbc4c12c71e375bcaf67b2396c3ce868a0c61
Evan Nguyen <vudutminh@gmail.com>
2026-08-09 23:17:02 +1000

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int ClimbStairs(int n) {     
+        int a=1, b=1;
+        for (int i=0; i< n-1;i++) {
+            int t = a;
+            a = a + b;
+            b = t;
+        }
+        return a;
+    }
+}
